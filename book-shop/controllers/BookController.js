550ec70f277eb525fdbe6be3faebcc9847d8939f
Baguette-bbang <dudals9701@naver.com>
2024-05-27 10:21:03 +0900

    Search for books by category & new books

--- book-shop/controllers/BookController.js
+++ book-shop/controllers/BookController.js
@@ -4,11 +4,38 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const getBooks = (req, res) => {
-    if (req.query.category_id) {
-        booksByCategory(req, res);
-    } else {
-        allBooks(req, res);
+    let {category_id, news, limit, currentPage} = req.query;
+    let offset = limit * (currentPage - 1);
+    let sql = `SELECT * FROM books`;
+    let values = [];
+    
+    if (category_id && news) {
+        sql += ` WHERE category_id = ? and pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
+        values = [category_id];
+    } 
+    else if (category_id) {
+        sql += ' WHERE category_id = ?';
+        values = [category_id];
+    } else if (news) {
+        sql += ` WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
     }
+
+    sql += ` LIMIT ? OFFSET ?`;
+    values.push(parseInt(limit), offset);
+
+    conn.query(sql, values, 
+        (err, results) => {
+            if(err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            if (results) {
+                return res.status(StatusCodes.OK).json(results);
+            } else {
+                return res.status(StatusCodes.NOT_FOUND).end();
+            }
+        }
+    );
 }
 
 const allBooks = (req, res) => {
@@ -32,7 +59,9 @@ const allBooks = (req, res) => {
 const bookDetail = (req, res) => {
     var {id} = req.params;
     id = parseInt(id);
-    let sql = `SELECT * FROM books WHERE id = ?`;
+    let sql = `SELECT * FROM books 
+                LEFT JOIN categories ON books.category_id = categories.id
+                WHERE books.id = ?`;
     let values = [id]
 
     conn.query(sql, values, 
@@ -46,7 +75,6 @@ const bookDetail = (req, res) => {
             } else {
                 return res.status(StatusCodes.NOT_FOUND).end();
             }
-            
         }
     );
 }
@@ -55,7 +83,7 @@ const booksByCategory = (req, res) => {
     let {category_id} = req.query;
     let sql = `SELECT * FROM books WHERE category_id = ?`;
     let values = [category_id];
-
+    
     conn.query(sql, values,
         (err, results) => {
             if(err) {
@@ -74,5 +102,6 @@ const booksByCategory = (req, res) => {
 
 module.exports = {
     getBooks,
-    bookDetail
+    bookDetail,
+    booksByCategory
 };
