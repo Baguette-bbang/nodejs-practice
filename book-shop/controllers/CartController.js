35cd0fe43c59665bff89aa9f5d019a74fc68c9c9
Baguette-bbang <dudals9701@naver.com>
2024-06-01 22:49:12 +0900

    feat: 장바구니 관련  API에 JWT 인증 추가 - JWT 토큰을 통한 사용자 인증 기능 추가 - 인증된 사용자의 ID를 기반으로 장바구니 관련 로직 구현 - 인증 실패 시 적절한 오류 메시지 반환 - 비동기화 처리 - try-catch-finally문 적용

--- book-shop/controllers/CartController.js
+++ book-shop/controllers/CartController.js
@@ -1,53 +1,73 @@
 const {StatusCodes} = require('http-status-codes');
-const conn = require('../db/mariadb');
+const connection = require('../db/mariadb');
+const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const addToCart = (req, res) => {
-    const {book_id, quantity, user_id} = req.body;
+const addToCart = async (req, res) => {
+    const conn = await connection();
+    const {book_id, quantity} = req.body;
+    
+    try {
+        const {user_id} = ensureAuthorization(req);    
+        let sql = "INSERT INTO cartItems (book_id, quantity, user_id) VALUES (?, ?, ?)";
+        let values = [book_id, quantity, user_id];
+        const results = await conn.execute(sql, values);
 
-    let sql = "INSERT INTO cartItems (book_id, quantity, user_id) VALUES (?, ?, ?)";
-    let values = [book_id, quantity, user_id];
+        return res.status(StatusCodes.OK).json(results[0]);
+    } catch (err) {
+        console.error(err);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+    } finally {
+        await conn.end(); // 연결 종료
+    }
+};
 
-    conn.query(sql, values, (err, results) => {
-        if (err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
+const getCartItem = async (req, res) => {
+    const conn = await connection();
+    const {selected} = req.body; // selected = [cartItemsId1, cartItemsId2...]
+
+    try {
+        const {user_id} = ensureAuthorization(req);
+        console.log(user_id);
+        console.log(selected);
+        let sql = `SELECT cartItems.id, book_id, title, summary, quantity, price, price*quantity as total_price
+                    FROM cartItems
+                    JOIN books ON(cartItems.book_id = books.id)
+                    WHERE user_id = ? AND cartItems.id IN (?)`;
+        let values = [user_id, selected];   
+
+        const [results] = await conn.query(sql, values);
         return res.status(StatusCodes.OK).json(results);
-    })
-}
+    } catch (err) {
+        console.error(err);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+    } finally {
+        await conn.end(); // 연결 종료
+    }
+};
 
-const getCartItem = (req, res) => {
-    const {user_id, selected} = req.body; // selected = [cartItemsId1, cartItemsId2...]
+const removeCartItem = async (req, res) => {
+    const conn = await connection();
+    const cartItemId = req.params.id;
+    try {
+        let sql = `DELETE FROM cartItems WHERE id = ?`
+        const [results] = await conn.execute(sql, cartItemId);
 
-    let sql = `SELECT cartItems.id, book_id, title, summary, quantity, price, price*quantity as total_price
-                FROM cartItems
-                JOIN books ON(cartItems.book_id = books.id)
-                WHERE user_id = ? AND cartItems.id IN (?)`;
-    let values = [user_id, selected];   
-
-    conn.query(sql, values, (err, results) => {
-        if (err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
         return res.status(StatusCodes.OK).json(results);
-    })
-}
+    } catch (err) {
+        console.error(err);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+    } finally {
+        await conn.end(); // 연결 종료
+    }
+};
 
-const removeCartItem = (req, res) => {
-    const {id} = req.params;
-    let sql = `DELETE FROM cartItems WHERE id = ?`
-    conn.query(sql, id,
-        (err, results) => {
-            if (err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
-            return res.status(StatusCodes.OK).json(results);
-        }
-    )
+const ensureAuthorization = (req) => {
+    let receivedJwt = req.headers["authorization"];
+    let decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
+
+    return decodedJwt
 }
 
 module.exports = {
