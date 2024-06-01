00a13c35891d75773a488f783608f72caad7a461
Baguette-bbang <dudals9701@naver.com>
2024-06-01 19:59:04 +0900

    feat: 좋아요 제거  API에 JWT 인증 추가
    - JWT 토큰을 통한 사용자 인증 기능 추가
    - 인증된 사용자의 ID를 기반으로 좋아요 제거  로직 구현
    - 인증 실패 시 적절한 오류 메시지 반환
    - 헤더에서 사용자의 ID를 추출하는 함수 구현
    - 좋아요 추가 API 리팩토링

--- book-shop/controllers/LikeController.js
+++ book-shop/controllers/LikeController.js
@@ -8,13 +8,10 @@ const addLike = async (req, res) => {
     const {id} = req.params;
     const conn = await connection();
     try {
-        let receivedJwt = req.headers["authorization"];
-        console.log(receivedJwt);
-        let decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
-        console.log(decodedJwt);
+        
+        const authorization = ensureAuthorization(req);
+        const {user_id} = authorization;
 
-        // decodedJwt에서 id 받아오기 
-        const {user_id} = decodedJwt;
         let sql = "INSERT INTO likes (user_id, book_id) VALUES (?, ?)";
         let values = [user_id, id];
 
@@ -26,20 +23,31 @@ const addLike = async (req, res) => {
     }
 };
 
-const removeLike = (req, res) => {
+const removeLike = async (req, res) => {
     const {id} = req.params;
-    const {user_id} = req.body;
+    const conn = await connection();
 
-    let sql = "DELETE FROM likes WHERE user_id = ? AND book_id = ?";
-    let values = [user_id, parseInt(id)];
-    conn.query(sql, values, (err, results) => {
-        if (err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
-        return res.status(StatusCodes.OK).json(results);
-    })
+    try {
+        const authorization = ensureAuthorization(req);
+        const {user_id} = authorization;
+
+        let sql = "DELETE FROM likes WHERE user_id = ? AND book_id = ?";
+        let values = [user_id, id];
+
+        const results = await conn.execute(sql, values)
+        return res.status(StatusCodes.OK).json(results[0]);
+    } catch (error) {
+        console.error(err);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+    }
 };
+
+const ensureAuthorization = (req) => {
+    let receivedJwt = req.headers["authorization"];
+    let decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
+
+    return decodedJwt
+}
 
 module.exports = {
     addLike,
