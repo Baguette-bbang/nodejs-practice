7ed2804c57cf1b7ede3c60cd7d3adc26fabc2d89
Baguette-bbang <dudals9701@naver.com>
2024-06-03 11:07:43 +0900

    feat : 사용자 장바구니 목록 조회와 선택한 장바구니 목록 조회 분리

--- book-shop/controllers/CartController.js
+++ book-shop/controllers/CartController.js
@@ -35,13 +35,17 @@ const getCartItem = async (req, res) => {
 
     try {
         const {user_id} = ensureAuthorization(req);
-        console.log(user_id);
-        console.log(selected);
+        // 기본적으로는 사용자 장바구니 목록 조회 (장바구니에 들어가는 가서 내가 고른 아이템들 조회하는 것)
         let sql = `SELECT cartItems.id, book_id, title, summary, quantity, price, price*quantity as total_price
-                    FROM cartItems
-                    JOIN books ON(cartItems.book_id = books.id)
-                    WHERE user_id = ? AND cartItems.id IN (?)`;
-        let values = [user_id, selected];   
+            FROM cartItems
+            JOIN books ON(cartItems.book_id = books.id)
+            WHERE user_id = ?`;
+        let values = [user_id];   
+
+        if (selected) { // 주문서 작성 시 '선택한 장바구니 목록 조회' (장바구니에서 주문할 아이템들을 체크하고 주문서를 작성하는 것)
+            sql += ` AND cartItems.id IN (?)`;
+            values.push(selected);
+        }
 
         const [results] = await conn.query(sql, values);
         return res.status(StatusCodes.OK).json(results);
